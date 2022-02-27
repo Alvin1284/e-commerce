3656092d222bf03f5a660c44513a04789f8674f4
Alvin1284 <81688085+Alvin1284@users.noreply.github.com>
2022-02-27 23:57:13 +0300

    changes

--- products/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField()
